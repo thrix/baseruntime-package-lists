655316e4f518afb5210c550a32b68585c30c4d98
Stephen Gallagher <sgallagh@redhat.com>
2017-04-12 15:56:28 -0400

    Fixes to mock_wrapper

--- mock_wrapper.sh
+++ mock_wrapper.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
+set -e
+
 mock -r ./fedora-26-multiarch.cfg init
 mock -r ./fedora-26-multiarch.cfg --chroot "mkdir -p /opt/srpm/output"
 mock -r ./fedora-26-multiarch.cfg --copyin ./recreate_srpm.sh /opt/srpm/recreate_srpm.sh
 
-mkdir -p output
 mock -r ./fedora-26-multiarch.cfg --cwd=/opt/srpm/output --chroot /opt/srpm/recreate_srpm.sh $1
+rm -Rf ./output
 mock -r ./fedora-26-multiarch.cfg --copyout /opt/srpm/output ./output
