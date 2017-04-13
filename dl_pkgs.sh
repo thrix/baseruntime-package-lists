f7c25c83a5b72f22fc37c74da2ca1452f1b02e91
Stephen Gallagher <sgallagh@redhat.com>
2017-04-12 21:49:08 -0400

    Add simple utility to pull down updated binary rpms

---
+++ dl_pkgs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+PROCESSORS=$(/usr/bin/getconf _NPROCESSORS_ONLN)
+
+if [ x$1 == x ]; then
+    echo "No NVR file provided"
+    exit 1
+else
+    nvrfile=$(realpath $1)
+fi
+
+if [ x$2 == x ]; then
+    release="test/26_Alpha"
+else
+    release=$2
+fi
+
+
+for arch in "x86_64" "i686" "armv7hl" "aarch64" "ppc64" "ppc64le"; do
+    mkdir -p repo/$release/
+    pushd repo/$release/$arch/os/
+    cat $nvrfile | xargs --max-procs=$PROCESSORS -I NVR \
+        koji download-build --arch=noarch --arch=$arch NVR
+    popd
+done
