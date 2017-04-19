139fd64756a50f8f1a03d5045c703d5d44d4e217
Stephen Gallagher <sgallagh@redhat.com>
2017-04-19 14:05:20 -0400

    mock_wrapper now supports rawhide and other versions

--- mock_wrapper.sh
+++ mock_wrapper.sh
@@ -1,17 +1,70 @@
 #!/usr/bin/bash
-# Usage: ./mock_wrapper.sh <NVR file>
+# Usage: ./mock_wrapper.sh <version> <NVR file>
 
 set -e
 PROCESSORS=$(/usr/bin/getconf _NPROCESSORS_ONLN)
 
-mock -r ./fedora-26-multiarch.cfg init
-mock -r ./fedora-26-multiarch.cfg --chroot "mkdir -p /opt/srpm/output"
-mock -r ./fedora-26-multiarch.cfg --copyin ./recreate_srpm.sh \
+RELEASEVER=$1
+VERNUM=$RELEASEVER
+if [ "$RELEASEVER" == "rawhide" ]; then
+    VERNUM=27
+fi
+
+cat > ./fedora-$RELEASEVER-multiarch.cfg << EOF
+
+config_opts['root'] = 'fedora-$RELEASEVER-srpm'
+config_opts['target_arch'] = 'x86_64'
+config_opts['legal_host_arches'] = ('x86_64')
+config_opts['chroot_setup_cmd'] = 'install bash fedora-release fedpkg gnupg2 git-core redhat-rpm-config rpm-build shadow-utils gawk koji glibc-minimal-langpack rpmdevtools'
+config_opts['dist'] = 'fc$VERNUM'  # only useful for --resultdir variable subst
+config_opts['extra_chroot_dirs'] = [ '/run/lock', ]
+config_opts['releasever'] = '$VERNUM'
+config_opts['package_manager'] = 'dnf'
+
+config_opts['yum.conf'] = """
+[main]
+keepcache=1
+debuglevel=2
+reposdir=/dev/null
+logfile=/var/log/yum.log
+retries=20
+obsoletes=1
+gpgcheck=0
+assumeyes=1
+syslog_ident=mock
+syslog_device=
+install_weak_deps=0
+metadata_expire=0
+mdpolicy=group:primary
+best=1
+
+# repos
+
+[fedora]
+name=fedora
+metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-$RELEASEVER&arch=\$basearch
+failovermethod=priority
+gpgkey=file:///usr/share/distribution-gpg-keys/fedora/RPM-GPG-KEY-fedora-$VERNUM-primary
+gpgcheck=1
+
+[updates]
+name=updates
+metalink=https://mirrors.fedoraproject.org/metalink?repo=updates-released-f$RELEASEVER&arch=\$basearch
+failovermethod=priority
+gpgkey=file:///usr/share/distribution-gpg-keys/fedora/RPM-GPG-KEY-fedora-$VERNUM-primary
+gpgcheck=1
+"""
+
+EOF
+
+mock -r ./fedora-$RELEASEVER-multiarch.cfg init
+mock -r ./fedora-$RELEASEVER-multiarch.cfg --chroot "mkdir -p /opt/srpm/output"
+mock -r ./fedora-$RELEASEVER-multiarch.cfg --copyin ./recreate_srpm.sh \
                                            /opt/srpm/recreate_srpm.sh
-mock -r ./fedora-26-multiarch.cfg --copyin $1 \
+mock -r ./fedora-$RELEASEVER-multiarch.cfg --copyin $2 \
                                            /opt/srpm/srpms.txt
-mock -r ./fedora-26-multiarch.cfg --cwd=/opt/srpm/output --chroot \
+mock -r ./fedora-$RELEASEVER-multiarch.cfg --cwd=/opt/srpm/output --chroot \
     "cat /opt/srpm/srpms.txt | xargs --max-procs=$PROCESSORS -I NVR \
     /opt/srpm/recreate_srpm.sh NVR"
 rm -Rf ./output
-mock -r ./fedora-26-multiarch.cfg --copyout /opt/srpm/output ./output
+mock -r ./fedora-$RELEASEVER-multiarch.cfg --copyout /opt/srpm/output ./output
