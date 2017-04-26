7b0e192495e9af40cf10cca92f8fd45101c2da9a
Stephen Gallagher <sgallagh@redhat.com>
2017-04-26 12:15:11 -0400

    rsync-push: trim out archful SRPMs on Rawhide
    We don't want to waste space on fedorapeople with packages that
    change constantly.

--- repo/rsync-push.sh
+++ repo/rsync-push.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 set -e
 
 if [ x$1 == x ]; then
@@ -8,17 +10,33 @@ else
     release=$1
 fi
 
+if [ x$2 == x ]; then
+    dest="$SCRIPT_DIR/$release"
+else
+    dest="$2/$release"
+fi
+
 # Make sure every directory is present, even if it is empty
 for arch in "x86_64" "i686" "armv7hl" "aarch64" "ppc64" "ppc64le"; do
-    mkdir -p $release/$arch/sources $release/$arch/os
-    createrepo_c $release/$arch/sources
-    createrepo_c $release/$arch/os
+    mkdir -p $dest/$arch/sources $dest/$arch/os
+done
+
+if [ "$release" == "rawhide" ]; then
+    # Remove the generated archful SRPMs to save space, since they change
+    # constantly on rawhide
+    find $dest -name "*override*.src.rpm" -exec rm {} \;
+fi
+
+# Generate or update all of the repodata
+for arch in "x86_64" "i686" "armv7hl" "aarch64" "ppc64" "ppc64le"; do
+    createrepo_c $dest/$arch/sources
+    createrepo_c $dest/$arch/os
 done
 
 # Set the group ownership to the modularity-wg group
-chgrp -R 189842 $release
+chgrp -R 189842 $dest
 
 # Any changes made in these directories should also set this group
-find $release -type d -exec chmod g+s {} \;
+find $dest -type d -exec chmod g+s {} \;
 
-rsync -avh --delete-before -e ssh  $release/ fedorapeople.org:/project/modularity/repos/fedora/gencore-override/$release
+rsync -avh --delete-before -e ssh  $dest/ fedorapeople.org:/project/modularity/repos/fedora/gencore-override/$release
