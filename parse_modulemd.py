a660a0c52e8f86d694cc75f381a858782b5a06cd
Stephen Gallagher <sgallagh@redhat.com>
2017-06-13 09:54:29 -0400

    Properly handle recursive deps
    Also add function to get build-deps

--- parse_modulemd.py
+++ parse_modulemd.py
@@ -31,7 +31,42 @@ def get_modulemd(module_name, stream):
     if not mod_info or "results" not in mod_info.keys() or not mod_info["results"]:
         raise IOError("%s (stream: %s) is not available on PDC" % (
                        module_name, stream))
-    return mod_info["results"][-1]["modulemd"]
+    return yaml.load(mod_info["results"][-1]["modulemd"], Loader=Loader)
+
+def _get_module_build_deps(name, ref, modulemd=None):
+    if not modulemd:
+        modulemd = get_modulemd(name, ref)
+    try:
+        module_deps = modulemd['data']['dependencies']['buildrequires']
+    except KeyError:
+        # This module has no dependencies
+        return {}
+
+    return module_deps
+
+def _get_module_deps(name, ref, modulemd=None):
+    if not modulemd:
+        modulemd = get_modulemd(name, ref)
+    try:
+        module_deps = modulemd['data']['dependencies']['requires']
+    except KeyError:
+        # This module has no dependencies
+        return {}
+
+    return module_deps
+
+def _get_recursive_module_deps(deps, name, ref):
+    if name in deps and deps[name] == ref:
+        return
+    if name in deps and not deps['name'] == ref:
+        raise TypeError("Conflicting refs for {}".format(name))
+
+    deps[name] = ref
+
+    module_deps = _get_module_deps(name, ref)
+
+    for depname, depref in module_deps.items():
+        _get_recursive_module_deps(deps, depname, depref)
 
 @click.group()
 @click.option('--module', default='base-runtime',
@@ -44,8 +79,11 @@ def get_modulemd(module_name, stream):
 def cli(ctx, module, modulemd, ref):
     if modulemd:
         ctx.obj["modulemd"] = yaml.load(open(modulemd, 'r'))
+        ctx.obj["name"] = ctx.obj["modulemd"]['data']['name']
     else:
-        ctx.obj["modulemd"] = yaml.load(get_modulemd(module, ref), Loader=Loader)
+        ctx.obj["modulemd"] = get_modulemd(module, ref)
+        ctx.obj["name"] = module
+    ctx.obj["ref"] = ref
 
 @cli.command()
 @click.pass_context
@@ -54,9 +92,32 @@ def get_api(ctx):
         print(rpm)
 
 @cli.command()
+@click.option('--recursive/--no-recursive', default=True,
+              help='Whether to get all of the dependencies of dependencies')
 @click.pass_context
-def get_deps(ctx):
-    deps = ctx.obj['modulemd']['data']['dependencies']['requires']
+def get_deps(ctx, recursive):
+    deps = _get_module_deps(ctx.obj["name"],
+                            ctx.obj["ref"],
+                            ctx.obj["modulemd"])
+
+    if recursive:
+        for depname, depref in deps.items():
+            _get_recursive_module_deps(deps, depname, depref)
+
+    for dep in sorted(deps):
+        print("{}:{}".format(dep,deps[dep]))
+
+@cli.command()
+@click.pass_context
+def get_build_deps(ctx):
+    deps = _get_module_build_deps(ctx.obj["name"],
+                                  ctx.obj["ref"],
+                                  ctx.obj["modulemd"])
+
+    # Get all the runtime dependencies for these build-deps
+    for depname, depref in deps.items():
+            _get_recursive_module_deps(deps, depname, depref)
+
     for dep in sorted(deps):
         print("{}:{}".format(dep,deps[dep]))
 
