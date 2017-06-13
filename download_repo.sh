7ef1f501e357d1987b6b983e23d1ac18c8fad0ca
Stephen Gallagher <sgallagh@redhat.com>
2017-06-13 10:40:30 -0400

    Get toplevel packages from modulemd

--- download_repo.sh
+++ download_repo.sh
@@ -318,7 +318,7 @@ for arch in ${_arg_arch[@]}; do
         # fedorapeople
         # This will pull down the full contents, so we can update
         # the repository as necessary
-        override_uri="rsync://fedorapeople.org/project/modularity/repos/fedora/gencore-override/${version_path}/$arch"
+        override_uri="rsync://fedorapeople.org/project/modularity/repos/fedora/gencore-override/${version_path}/$arch/"
 
         dest_override="$(readlink -f ${_arg_repo_path})/${version_path}/override/$arch"
         mkdir -p $dest_override
